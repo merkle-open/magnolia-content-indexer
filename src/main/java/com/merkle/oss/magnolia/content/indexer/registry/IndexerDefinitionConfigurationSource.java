b255fb79c90cad140b421c1ad1ebf19f5a279b48
eschleb <bjoern.eschle@merkle.com>
2025-09-03 11:49:29 +0200

    upgrade to mgnl 6.4 (javax -> jakarta)

--- src/main/java/com/merkle/oss/magnolia/content/indexer/registry/IndexerDefinitionConfigurationSource.java
+++ src/main/java/com/merkle/oss/magnolia/content/indexer/registry/IndexerDefinitionConfigurationSource.java
@@ -6,7 +6,7 @@ import java.lang.invoke.MethodHandles;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
