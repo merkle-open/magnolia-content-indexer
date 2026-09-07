b6cd3e861967e35e35ed3e4d279134ff6782ee0a
eschleb <bjoern.eschle@merkle.com>
2026-09-07 11:30:30 +0200

    Fix move not reindexing node
     - Only filter out activation properties, not all magnolia properties

--- src/main/java/com/merkle/oss/magnolia/content/indexer/annotation/EventPredicate.java
+++ src/main/java/com/merkle/oss/magnolia/content/indexer/annotation/EventPredicate.java
@@ -1,5 +1,6 @@
 package com.merkle.oss.magnolia.content.indexer.annotation;
 
+import info.magnolia.cms.util.FilteredEventListener;
 import info.magnolia.jcr.util.NodeTypes;
 
 import java.lang.invoke.MethodHandles;
@@ -19,15 +20,14 @@ public interface EventPredicate extends Predicate<Event> {
     class SystemExcludingPredicates implements EventPredicate {
         private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
         private static final Set<String> BLACKLIST_PROPERTY_PREFIX = Set.of(
-                NodeTypes.JCR_PREFIX,
-                NodeTypes.MGNL_PREFIX
+                NodeTypes.JCR_PREFIX
         );
         private static final Pattern BLACKLIST_PATTERN = Pattern.compile("/("+ String.join("|", BLACKLIST_PROPERTY_PREFIX) +")[^/]+$");
         @Override
         public boolean test(final Event event) {
             try {
                 final String path = event.getPath();
-                return !path.startsWith("/"+ JcrConstants.JCR_SYSTEM) && !BLACKLIST_PATTERN.matcher(path).find();
+                return !path.startsWith("/"+ JcrConstants.JCR_SYSTEM) && FilteredEventListener.ACTIVATION_EXCLUDING_PREDICATE.evaluate(event) && !BLACKLIST_PATTERN.matcher(path).find();
             } catch (Exception e) {
                 LOG.error("Could not get path of event {}.", event);
                 return false;
